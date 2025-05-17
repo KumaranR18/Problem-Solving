67afcc21ef40607f1dee99b2f190b81c969f72f4
KumaranR18 <143732898+KumaranR18@users.noreply.github.com>
2025-05-17 18:13:43 +0530

    Update Missing in Array.java

--- Missing in Array.java
+++ Missing in Array.java
@@ -1,12 +1,11 @@
 class Solution {
-    int missingNum(int arr[]) {
-        int n, sum2 = 0;
-        long sum1;
-        n = arr.length + 1;
-        sum1 = (long) n*(n+1)/2;
-        for (int i=0; i<arr.length; i++){
-            sum2 += arr[i];
+    public int missingNumber(int[] nums) {
+        int n, sum1, sum2 = 0;
+        n = nums.length;
+        sum1 = n*(n+1)/2;
+        for (int i=0; i<nums.length; i++){
+            sum2 += nums[i];
         }
-        return (int) sum1 - sum2;
+        return sum1 - sum2;
     }
 }
