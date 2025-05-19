b8c6919e13183ad4daaf0ed7112ef4e0e798a7bc
KumaranR18 <143732898+KumaranR18@users.noreply.github.com>
2025-05-20 01:29:25 +0530

    Add files via upload

--- Prime Numbers Within a Given Range.java
+++ Prime Numbers Within a Given Range.java
@@ -1,9 +1,11 @@
-Prime Numbers Up to N
+Finding Prime Numbers Within a Given Range
 
 Example:
 
-Enter the number: 40
-Prime numbers till 40 : 2 3 5 7 11 13 17 19 23 29 31 37 
+Enter the Starting number: 1
+Enter the Ending number: 36
+Prime numbers between 1 and 36 are: 2 3 5 7 11 13 17 19 23 29 31 
+
 
 
 import java.util.*;
@@ -11,11 +13,13 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-        System.out.print("Prime numbers till " + n + " : " );
+        System.out.print("Enter the Starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter the Ending number: ");
+        int end = sc.nextInt();
+        System.out.print("Prime numbers between " + start + " and " + end + " are: ");
 
-        for (int num=1; num<=n; num++) {
+        for (int num=start; num<=end; num++) {
             boolean flag = true;
             for (int i=2; i<=num/2; i++) {
                 if (num % i == 0) {
