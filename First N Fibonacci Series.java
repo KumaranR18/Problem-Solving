1ab7e579223825104d24a81df9f3c86fe7bb6725
KumaranR18 <143732898+KumaranR18@users.noreply.github.com>
2025-05-21 01:56:23 +0530

    Add files via upload

--- First N Fibonacci Series.java
+++ First N Fibonacci Series.java
@@ -1,4 +1,4 @@
-First N Fibonacci Series
+a. First N Fibonacci Series
 
 Example:
 
@@ -23,3 +23,37 @@ class Main {
         }
     }
 }
+
+
+
+b. First n Fibonacci
+
+Given a number n, return an array containing the first n Fibonacci numbers.
+
+Note: The first two numbers of the series are 0 and 1.
+
+Examples:
+
+Input: n = 5
+Output: [0, 1, 1, 2, 3]
+
+Input: n = 7
+Output: [0, 1, 1, 2, 3, 5, 8]
+
+Input: n = 2
+Output: [0, 1]
+
+class Solution {
+    public static int[] fibonacciNumbers(int n) {
+        int arr[] = new int[n];
+        arr[0] = 0;
+        if (n == 1) return arr;
+        arr[1] = 1;
+        if (n == 2) return arr;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        return arr;
+    }
+}
+
