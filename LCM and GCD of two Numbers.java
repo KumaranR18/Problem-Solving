1c21950483b1a6111597960d279b4255337f38e1
Kumaran R <143732898+KumaranR18@users.noreply.github.com>
2025-05-23 17:19:41 +0530

    Add files via upload

---
+++ LCM and GCD of two Numbers.java
@@ -0,0 +1,57 @@
+a. LCM of two Numbers
+
+Example:
+
+Enter the Number 1: 6
+Enter the Number 2: 2
+LCM: 6
+
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the Number 2: ");
+        int b = sc.nextInt();
+        int greater = a>b ? a:b;
+        while(true){
+            if (greater % a == 0 && greater % b == 0){
+                System.out.println("LCM: " + greater);
+                break;
+            }
+            greater++;
+        }
+    }
+}
+
+
+
+b. GCD of two Numbers
+
+Example:
+
+Enter the Number 1: 6
+Enter the Number 2: 2
+GCD: 2
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the Number 2: ");
+        int b = sc.nextInt();
+        int lesser = a<b ? a:b;
+        while(true){
+            if (a % lesser == 0 && b % lesser == 0){
+                System.out.println("GCD: " + lesser);
+                break;
+            }
+            lesser++;
+        }
+    }
+}
+
