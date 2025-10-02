0c3f5adc282d2256dcae2774eaf822b0ce427431
Kumaran R <143732898+KumaranR18@users.noreply.github.com>
2025-10-02 21:46:52 +0530

    Add files via upload

---
+++ Searching in an Array Using Collections.java
@@ -0,0 +1,38 @@
+Searching in an Array Using Collections
+
+Given an integer k and array arr. Your task is to return the position of the first occurrence of k in the given array and if element k is not present in the array then return -1.
+
+Note: 1-based indexing is followed here.
+
+Examples:
+
+Input: k = 16 , arr = [9, 7, 16, 16, 4]
+Output: 3
+Explanation: The value 16 is found in the given array at positions 3 and 4, with position 3 being the first occurrence.
+
+Input: k=98 , arr = [1, 22, 57, 47, 34, 18, 66]
+Output: -1
+Explanation: k = 98 isn't found in the given array.
+
+Constraints:
+1 <= arr.size <= 106
+1 <= arr[i] <= 109
+1 <= k <= 106
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+
+class Solution {
+    public int search(int k, ArrayList<Integer> arr) {
+        for(int i=0; i<arr.size(); i++){
+            if(arr.get(i) == k){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
